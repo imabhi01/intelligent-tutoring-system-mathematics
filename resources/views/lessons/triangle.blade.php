9a4557118913cb4fe77dded4cdb406f4b938b434
Abhishek Chaudhary <iamdev.abhishek@gmail.com>
2023-12-09 09:44:24 +0000

    Lessons/courses added

--- resources/views/lessons/triangle.blade.php
+++ resources/views/lessons/triangle.blade.php
@@ -11,41 +11,29 @@
         <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
             <div class="mx-auto">
                 <section class="px-5 py-5 mx-auto">
-                    <img src="/images/circle.png" width="200" height="200" alt="circle-image" style="margin: 0 auto">
-                    <h2 class="text-2xl font-semibold mb-3">Circle</h2>
-                    <p class="text-lg">A circle is a geometric shape that can be fully enclosed by a single straight line, called the circumference of the circle. It can be also considered as the set of all points that are at a constant distance from a fixed point, the center of the circle.</p>
+                    <img src="/images/triangle.png" width="450" height="450" alt="circle-image" style="margin: 0 auto">
+                    <h2 class="text-2xl font-semibold mb-3">Area of Triangle</h2>
+                    <p class="text-lg">The area of a triangle can be calculated using various formulas. For example, Heron’s formula is used to calculate the triangle’s area, when we know the length of all three sides. Trigonometric functions are also used to find the area of a triangle when we know two sides and the angle formed between them. However, the basic formula that is used to find the area of a triangle is:</p>
+                    <br>
+                    <p class="text-center"><strong>Area of triangle = 1/2 × base × height</strong></p>
                 </section>
 
                 <section class="px-5 py-5 mx-auto">
-                    <h2 class="text-2xl font-semibold mb-3">Radius and Diameter</h2>
-                    <p class="text-lg">The radius of a circle is a line segment from the center of the circle to any point on the circumference of the circle. The diameter of a circle is a line segment that passes through the center of the circle and is equal to twice the radius of the circle.</p>
-                </section>
-
-                <section class="px-5 py-5 mx-auto">
-                    <h2 class="text-2xl font-semibold mb-3">Formula for Area and Circumference</h2>
-                    <p class="text-lg">Area of a circle: A = πr²</p>
-                    <p class="text-lg">
-                        Circumference of Circle: C = 2πr
-                    </p>
-                    <p class="text-lg">
-                        Diameter of Circle: C = 2r
-                    </p>
-                    <p class="text-lg">
-                       Where, π value is 22/7 = 3.14
-                    </p>
-                </section>
-                
-                <section class="px-5 py-5 mx-auto">
-                    <h2 class="text-2xl font-semibold mb-3">Calculate Area, Circumference and Diameter of a circle of radius = 7.</h2>
-                    <p class="text-lg">Area of Circle: A = πr² = 22/7 * 7² = 154 </p>
-                    <p class="text-lg">Circumference of Circle: C = 2πr = 2 * 22/7 * 7 = 44 </p>
-                    <p class="text-lg">Diameter of Circle: C = 2r = 2 * 7 = 14 </p>
+                    <h2 class="text-2xl font-semibold mb-3">Perimeter of a Triangle</h2>
+                    <img src="/images/perimeter.png" width="450" height="450" alt="perimeter" style="margin: 0 auto">
+                    <p class="text-lg">The perimeter of a triangle is defined as the total length of the boundary. Since the boundary of the triangle is made by three line segments which are its sides, the perimeter of the triangle is equal to the sum of all three sides.</p>
+                    <p class="text-lg">P= a+b+c. Where, a, b, c indicates the sides of the triangle. One such example is when given sides are; a=5 cm, b=4 cm, c=6 cm. So we should add all the sides and hence the perimeter is 5+4+6= 15 cm.</p>
                 </section>
 
                 <section class="px-5 py-5 mx-auto">
                     <h2 class="text-2xl font-semibold mb-3">Video Tutorial</h2>
                     <div class="aspect-w-16 aspect-h-9">
-                    <iframe width="900" height="515" src="https://www.youtube.com/embed/D4nGkWOPb6M?si=vuCxKkia313x1Dbh" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+                    <h3 class="text-2xl font-semibold mb-3">Area of Triangle</h3>
+                    <iframe width="900" height="515" src="https://www.youtube.com/embed/6WsQH1cAYMM?si=vpzG38dfnJr9akRV" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+                    </div>
+                    <div class="aspect-w-16 aspect-h-9">
+                    <h3 class="text-2xl font-semibold mb-3">Perimeter of a Triangle</h3>
+                    <iframe width="900" height="515" src="https://www.youtube.com/embed/puPNJgqvbG0?si=jUkMrfwcpokLKSoD" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
                     </div>
                 </section>
             </div>
