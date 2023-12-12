876641b3a89b74f43660d440ca9984c006b2783f
Abhishek Chaudhary <iamdev.abhishek@gmail.com>
2023-12-12 17:10:20 +0000

    triangle lessons images

--- resources/views/lessons/triangle.blade.php
+++ resources/views/lessons/triangle.blade.php
@@ -11,7 +11,7 @@
         <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
             <div class="mx-auto">
                 <section class="px-5 py-5 mx-auto">
-                    <img src="/images/triangle.png" width="450" height="450" alt="triangle-image" style="margin: 0 auto">
+                    <img src="/images/triangle.PNG" width="450" height="450" alt="triangle-image" style="margin: 0 auto">
                     <h2 class="text-2xl font-semibold mb-3">Area of Triangle</h2>
                     <p class="text-lg">The area of a triangle can be calculated using various formulas. For example, Heron’s formula is used to calculate the triangle’s area, when we know the length of all three sides. Trigonometric functions are also used to find the area of a triangle when we know two sides and the angle formed between them. However, the basic formula that is used to find the area of a triangle is:</p>
                     <br>
@@ -20,7 +20,7 @@
 
                 <section class="px-5 py-5 mx-auto">
                     <h2 class="text-2xl font-semibold mb-3">Perimeter of a Triangle</h2>
-                    <img src="/images/perimeter.png" width="450" height="450" alt="perimeter" style="margin: 0 auto">
+                    <img src="/images/perimeter.PNG" width="450" height="450" alt="perimeter" style="margin: 0 auto">
                     <p class="text-lg">The perimeter of a triangle is defined as the total length of the boundary. Since the boundary of the triangle is made by three line segments which are its sides, the perimeter of the triangle is equal to the sum of all three sides.</p>
                     <p class="text-lg">P= a+b+c. Where, a, b, c indicates the sides of the triangle. One such example is when given sides are; a=5 cm, b=4 cm, c=6 cm. So we should add all the sides and hence the perimeter is 5+4+6= 15 cm.</p>
                 </section>
