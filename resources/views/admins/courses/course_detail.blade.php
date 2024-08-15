8027192ed84d6f500ef24ebfd0813b3271df995b
Abhishek Chaudhary <iamdev.abhishek@gmail.com>
2024-08-15 15:41:13 +0100

    push all

--- resources/views/admins/courses/course_detail.blade.php
+++ resources/views/admins/courses/course_detail.blade.php
@@ -7,7 +7,6 @@
         </div>
     </x-slot>
 
-
     <div class="max-w-7xl m-4 mx-auto sm:px-6 lg:px-8">
         <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
             <div class="container">
